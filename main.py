2dac82d474122e04f94af22bc5380223fa819a69
bobcat <armandjr@live.nl>
2021-01-07 14:32:38 +0100

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from code.classes import district, house, battery
+
+if _name__ == "__main__":
+
+    data_district = "data/district_{variable}/district-{variable}_houses.csv"
+    data_batteries = "data/district_{variable}/district-{variable}_batteries.csv"
+    
