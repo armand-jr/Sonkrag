f5f55da0371790116e720e3335f11ddafce3747f
bobcat <armandjr@live.nl>
2021-01-07 14:33:37 +0100

    update

--- code/classes/district.py
+++ code/classes/district.py
@@ -7,8 +7,21 @@ class District ():
 
     # TODO initialize
     def __init__(self, source_battery, source_house):
-        pass
+        self.houses = self.load_houses(source_house)
+        self.batteries = self.load_batteries(source_battery)
+        
 
     # open csv file
-    def load_houses(self):
-        pass
+    def load_houses(self, source_house):
+        """
+        load all houses
+        """
+        with open(source_house, 'r'):
+            
+
+
+    def load_batteries(self, source_battery):
+        """
+        load all batteries
+        """
+        with open(source_battery, 'r'):
